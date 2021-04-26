ec8516ba6c4c60b61f8656258639f5b8e6914083
CristianSanchez <cristiansanchezbarba@gmail.com>
2021-04-26 11:21:53 +0200

    I introduced functions buttons and some classes styles for project area, and it was created the isotope file and the code for it inside the main.js file

--- js/main.js
+++ js/main.js
@@ -1,5 +1,4 @@
 $(document).ready(function(){
-
     let $btns = $('.project-area .button-group button');
 
     $btns.click(function(e){
@@ -7,51 +6,11 @@ $(document).ready(function(){
         e.target.classList.add('active');
 
         let selector = $(e.target).attr('data-filter');
-        $('.project-area .grid').isotope({
+        $('.project-area .flex-container').isotope({
             filter : selector
         });
 
         return false;
+
     })
-
-    $('.project-area .button-group #btn1').trigger('click');
-
-
-
-
-
-    // owl-carousel
-    $(".site-main .about-area .owl-carousel").owlCarousel({
-        loop: true,
-        autoplay:true,
-        dots: true,
-        responsive:{
-            0:{
-                items: 1
-            },
-            544:{
-                items:2
-            }
-        }
-    })
-
-
-//sticky navigation menu//
-
-let nav_offset_top = $('header_area').height() + 50;
-
-function navbarFixed(){
-    if($('.header_area').lenght){
-        $(window).scroll(function(){
-            let scroll = $(window).scrollTop();
-            if(scroll >= nav_offset_top){
-                $('.header_area .main-menu').addClass('navbar_fixed');
-            }else{
-                $('.header_area .main-menu').removeClass('navbar_fixed');
-            }
-        })
-    }
-}
-
-navbarFixed();
 });
