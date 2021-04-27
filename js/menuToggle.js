2f31efa00e66619ae28ba21cbd85e99928c8f472
CristianSanchez <cristiansanchezbarba@gmail.com>
2021-04-27 18:16:28 +0200

    I added the nav and some classes. Also I added the script for menu

---
+++ js/menuToggle.js
@@ -0,0 +1,6 @@
+function toggleMenu(){
+    document.getElementById("primaryNav").classList.toggle("open");
+}
+
+let x = document = document.getElementById('hamburgerBtn');
+x.onclick = toggleMenu;
