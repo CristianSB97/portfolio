eb634f5c4bd2a6d5b36e4660639b8b2de513e946
CristianSanchez <cristiansanchezbarba@gmail.com>
2021-05-28 18:31:11 +0200

    total change portfolio

--- js/menuToggle.js
+++ js/menuToggle.js
@@ -1,25 +1,12 @@
-function toggleMenu(){
-    document.getElementById("primaryNav").classList.toggle("open");
-}
-
-let x = document = document.getElementById('hamburgerBtn');
-x.onclick = toggleMenu;
-
-
-// Fixed Nav
- 
-// var scrollBefore = 0;
-// window.addEventListener('scroll', function(e){
-//    const scrolled = window.scrollY;
-//    if(scrollBefore > scrolled){
-//        console.log("ScrollUP");
-//        scrollBefore = scrolled;
-//        document.getElementById("navbar").className =document.getElementById("navbar").className.replace(/(?:^|\s)fixed-nav(?!\S)/g , '');
-//    }else{
-//        scrollBefore = scrolled;
-//        console.log("ScrollDOWN");
-//        document.getElementById("navbar").className += " fixed-nav";
-//    }
-//}) 
-
+$(function() {
+    $(".toggle").on("click", function() {
+        if ($(".item").hasClass("active")) {
+            $(".item").removeClass("active");
+            $(this).find("a").html("<i class='fas fa-bars'></i>");
+        } else {
+            $(".item").addClass("active");
+            $(this).find("a").html("<i class='fas fa-times'></i>");
+        }
+    });
+});
 
